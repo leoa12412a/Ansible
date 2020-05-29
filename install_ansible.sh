4942a87df8f179947f3206433598ec5bbbebce7e
leoa12412a <leoa12412a@gmail.com>
2020-05-29 16:42:32 +0800

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,4 +1,4 @@
-yum update -y
-yum install -y epel-release
-yum install -y ansible
-yum install -y python-pip
+yum update -y;
+yum install -y epel-release;
+yum install -y ansible;
+yum install -y python-pip;
